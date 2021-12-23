fd16464a555bf5c8d32f2529dee8a38042f1aec3
Debadri-Redapple <debadri@redappletech.com>
2021-12-23 12:51:39 +0530

    Updated package to version 1.1.0

---
+++ Runtime/Plugins/iOS/mikros_framework_ios.framework/Headers/mikros_framework_ios.h
@@ -0,0 +1,13 @@
+//
+// Copyright 2013-present Tatum Games, LLC.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for mikros_framework_ios.
+FOUNDATION_EXPORT double mikros_framework_iosVersionNumber;
+
+//! Project version string for mikros_framework_ios.
+FOUNDATION_EXPORT const unsigned char mikros_framework_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mikros_framework_ios/PublicHeader.h>
